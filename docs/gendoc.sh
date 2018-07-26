2b10c09a5d0faca5ed17f340cd7ab4a9beab64dd
Jordan Irwin <antumdeluge@gmail.com>
2018-07-26 14:58:12 -0700

    Change "File" & "Source" to "Category" from helper script

--- docs/gendoc.sh
+++ docs/gendoc.sh
@@ -16,20 +16,34 @@ fi
 DOCS="$(dirname $(readlink -f $0))"
 ROOT="$(dirname ${DOCS})"
 CONFIG="${DOCS}/config.ld"
+TARGET="${DOCS}/api"
 
 cd "${ROOT}"
 
 # Clean old files
 echo -e "\nCleaning old files ..."
-rm -rf "${DOCS}/api.html" "${DOCS}/scripts" "${DOCS}/source" "${DOCS}/modules" "${DOCS}/topics" "${DOCS}/ldoc.css"
+rm -rf "${TARGET}.html" "${DOCS}/scripts" "${DOCS}/source" "${DOCS}/modules" "${DOCS}/topics" "${DOCS}/ldoc.css"
 
 if [ "${PARAM}" != "clean" ]; then
 	# Create new files
 	echo -e "\nGenerating new documentation ..."
-	ldoc -c "${CONFIG}" -d "${DOCS}" "${DOCS}/api.ldoc"
-	
-	if [ -f "${DOCS}/api.html" ]; then
+	ldoc -c "${CONFIG}" -d "${DOCS}" "${TARGET}.ldoc"
+
+	echo -e "\nMaking some final adjustments ..."
+
+	if [ -f "${TARGET}.html" ]; then
 		# Put "prefix:name_" settings in angled brackets (<>)
-		sed -i -e 's|>prefix:name|>\&lt;prefix\&gt;:\&lt;name\&gt;|' "${DOCS}/api.html"
+		sed -i -e 's|>prefix:name|>\&lt;prefix\&gt;:\&lt;name\&gt;|' "${TARGET}.html"
 	fi
+
+	# Rename section "Source" to "Category"
+	for F in $(find "${DOCS}" -type f -name "*.html"); do
+		sed -i -e 's|<h2>Source</h2>|<h2>Category</h2>|' "${F}"
+	done
+
+	for F in $(find "${DOCS}/source" -type f -name "*.html"); do
+		sed -i -e 's|<h1>File |<h1>Category |' "${F}"
+	done
 fi
+
+echo -e "Done!"
